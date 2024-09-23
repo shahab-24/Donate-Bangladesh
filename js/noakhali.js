3b3696a4d583da2cfc2353c7cf734a3678e598b3
Shahab Uddin <shahab.82946@gmail.com>
2024-09-24 00:15:57 +0600

    all cards functionality done

--- js/noakhali.js
+++ js/noakhali.js
@@ -108,3 +108,59 @@ Swal.fire({
  
   
 })
+
+
+
+
+
+// Quota injured---
+
+document.getElementById('quota_donation').addEventListener('click', function(){
+  console.log('connected from noakhali');
+
+  // const inputValue = document.getElementById('noakhali_input').value;
+  // const inputAmount = parseFloat(inputValue);
+
+  const inputAmount = getInputValue('quota_input')
+  if(inputAmount === 'number' || inputAmount > 0){
+    const areaWiseAmount = getInnerText('quota_donation_amount')
+
+
+    const totalAmount = inputAmount + areaWiseAmount;
+    console.log(totalAmount);
+    
+    const finalTotal = initialSum - totalAmount;
+    console.log(finalTotal);
+    
+  
+  
+    document.getElementById('feni_donation_amount').innerText = totalAmount;
+    document.getElementById('initial_amount').innerText = finalTotal;
+    Swal.fire({
+      position: "top-end",
+      icon: "success",
+      title: `Thanks For Your Donation!
+       You Have Donated ${inputAmount} BDT`,
+      showConfirmButton: false,
+      timer: 1500
+    });
+    
+  } else{
+    
+Swal.fire({
+  icon: "error",
+  title: "Oops...",
+  text: "Please Put A Valid Number or Valid Amount!",
+  footer: '<a href="#">Recheck Your Input</a>'
+});
+    return;
+  }
+  
+
+  // const noakhaliInitial = document.getElementById('noakhali_donation_amount').innerText;
+  // const noakhaliAmount = parseFloat(noakhaliInitial)
+  
+
+ 
+  
+})
